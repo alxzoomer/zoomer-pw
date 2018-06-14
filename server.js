1eadbbe9b68d7a4299d6d9ccd04da2a3e3b92ea4
Alexey Nikitin <alexeyn@cqg.com>
2018-06-14 18:16:26 +0400

    Added eslint. Added docker cmd for sh

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const logger = require('morgan')
-const favicon = require('serve-favicon')
+const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
+const logger = require('morgan');
+const favicon = require('serve-favicon');
+
+const app = express();
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-app.use(favicon(path.join(__dirname, 'public', 'static', 'images', 'favicon.ico')))
-app.use(express.static(path.join(__dirname, 'public', 'static')))
+app.use(helmet());
+app.use(favicon(path.join(__dirname, 'public', 'static', 'images', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public', 'static')));
 
 app.use(logger('common'));
 
-app.listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}`))
+app.listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}`));
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:' + PORT)
+  res.setHeader('Access-Control-Allow-Origin', `http://localhost:${PORT}`);
 
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
   // Pass to next layer of middleware
-  next()
-})
+  next();
+});
 
-app.get('/', function (req, res) {
+app.get('/health-check', (_, res) => res.sendStatus(200));
+
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', '/index.html'));
 });
